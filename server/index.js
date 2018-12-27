378a65070f05a3d185d5f9d7b246750167bcbb28
Richard Ascanio <rascanio97@gmail.com>
2018-12-26 22:26:37 -0400

    adding cors

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 const { mongoose } = require('./database');
 
@@ -9,6 +10,9 @@ app.set('port', process.env.PORT || 3000);
 // Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:4200'
+}));
 
 // Routes
 app.use('/api/employees', require('./routes/employee.routes'));
